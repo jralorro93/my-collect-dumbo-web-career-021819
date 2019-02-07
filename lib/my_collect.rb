2f2bc6f9107a3a0c111a71d011b18a0651b8478c
Juan Alorro <jralorro93@gmail.com>
2019-02-07 15:39:19 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,11 @@
+require 'pry'
+
 def my_collect(array)
   i = 0
   new_array = []
   while i < array.length 
-    yield array[i]
+  binding.pry 
+    yield array[i] 
     i = i + 1
   end 
 end 
-new_array << 
